16df7261c02868046b87576ec269a0968c4315a6
kpatel114 <kpatel114@hawk.iit.edu>
2024-01-23 15:34:05 -0600

    added new styling to signin page

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -8,7 +8,7 @@ export const Signin = () => {
       className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('../images/background.jpg')"}}>
         <Navbar/>
         <div className=" bg-cover bg-amber-950 flex items-center justify-center h-full text-white text-center">
-          <div className="my-8 ">
+          <div className="my-8 max-w-md mx-auto p-8 bg-opacity-75 bg-black rounded-lg">
             <h1 className="text-4xl font-bold mb-4">Enter the Wispy Whiskey</h1>
             <p className="text-lg mb-8">Explore and discover new flavors of your favorite drank bro boss!</p>
           </div>
